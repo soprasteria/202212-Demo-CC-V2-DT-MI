50db825bbb921c8fc4a15b357684dfdc62ae460c
Fleury Renaud <renaud.fleury@soprasteria.com>
2022-12-07 15:09:58 +0100

    Mise à jour de Keycloak 20.0.1

--- step1-get-keycloak-and-deploy-apps-backend-squeletton.sh
+++ step1-get-keycloak-and-deploy-apps-backend-squeletton.sh
@@ -3,20 +3,20 @@ mkdir repo
 cd repo
 
 # Get lastest Keycloak release
-wget https://github.com/keycloak/keycloak/releases/download/17.0.1/keycloak-17.0.1.zip
+wget https://github.com/keycloak/keycloak/releases/download/20.0.1/keycloak-20.0.1.zip
 
 # Extract the archive
-unzip keycloak-17.0.1.zip
+unzip keycloak-20.0.1.zip
 
 # Go to the folder
-cd keycloak-17.0.1
+cd keycloak-20.0.1
 
 # Create the jar application
 clever create --type jar keycloak-server
-clever env set KEYCLOAK_DOMAIN $(clever domain | xargs)
 clever link keycloak-server
 # Create the PG addon
 clever addon create postgresql-addon --plan dev --addon-version 11 keycloak-postgres
 
 # Link the addon
 clever service link-addon keycloak-postgres
+clever env set KEYCLOAK_DOMAIN $(clever domain | xargs)
